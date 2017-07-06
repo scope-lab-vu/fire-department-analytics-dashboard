c137b8627a85893e7b420174beea1f797c502860
ZilinWang <zilin.wang@vanderbilt.edu>
2017-07-06 12:34:05 -0500

    Add files via upload

---
+++ myapp/templates/stations.js
@@ -0,0 +1,194 @@
+/**
+ * Created by wangshibao on 6/28/17.
+ */
+var stations = {
+    1: {
+        number: 1,
+        address: "130 Broadmoor Avenue",
+        city: "Nashville",
+        zip: 37207
+    },
+    2: {
+        number: 1,
+        address: "130 Broadmoor Avenue",
+        city: "Nashville",
+        zip: 37207
+
+    },
+    3: {
+        number: 1,
+        address: "130 Broadmoor Avenue",
+        city: "Nashville",
+        zip: 37207
+    },
+    4: {
+        number: 1,
+        address: "130 Broadmoor Avenue",
+        city: "Nashville",
+        zip: 37207
+    },
+    5: {
+        number: 1,
+        address: "130 Broadmoor Avenue",
+        city: "Nashville",
+        zip: 37207
+    },
+    6: {
+        number: 1,
+        address: "130 Broadmoor Avenue",
+        city: "Nashville",
+        zip: 37207
+    },
+    7: {
+        number: 1,
+        address: "130 Broadmoor Avenue",
+        city: "Nashville",
+        zip: 37207
+    },
+    8: {
+        number: 1,
+        address: "130 Broadmoor Avenue",
+        city: "Nashville",
+        zip: 37207
+    },
+    9: {
+        number: 1,
+        address: "130 Broadmoor Avenue",
+        city: "Nashville",
+        zip: 37207
+    },
+    10: {
+        number: 1,
+        address: "130 Broadmoor Avenue",
+        city: "Nashville",
+        zip: 37207
+    },
+    11: {
+        number: 1,
+        address: "130 Broadmoor Avenue",
+        city: "Nashville",
+        zip: 37207
+    },
+    12: {
+        number: 1,
+        address: "130 Broadmoor Avenue",
+        city: "Nashville",
+        zip: 37207
+    },
+    13: {
+        number: 1,
+        address: "130 Broadmoor Avenue",
+        city: "Nashville",
+        zip: 37207
+    },
+    14: {
+        number: 1,
+        address: "130 Broadmoor Avenue",
+        city: "Nashville",
+        zip: 37207
+    },
+    15: {
+        number: 1,
+        address: "130 Broadmoor Avenue",
+        city: "Nashville",
+        zip: 37207
+    },
+    16: {
+        number: 1,
+        address: "130 Broadmoor Avenue",
+        city: "Nashville",
+        zip: 37207
+    },
+    17: {
+        number: 1,
+        address: "130 Broadmoor Avenue",
+        city: "Nashville",
+        zip: 37207
+    },
+    18: {
+        number: 1,
+        address: "130 Broadmoor Avenue",
+        city: "Nashville",
+        zip: 37207
+    },
+    19: {
+        number: 1,
+        address: "130 Broadmoor Avenue",
+        city: "Nashville",
+        zip: 37207
+    },
+    20: {
+        number: 1,
+        address: "130 Broadmoor Avenue",
+        city: "Nashville",
+        zip: 37207
+    },
+    21: {
+        number: 1,
+        address: "130 Broadmoor Avenue",
+        city: "Nashville",
+        zip: 37207
+    },
+    22: {
+        number: 1,
+        address: "130 Broadmoor Avenue",
+        city: "Nashville",
+        zip: 37207
+    },
+    23: {
+        number: 1,
+        address: "130 Broadmoor Avenue",
+        city: "Nashville",
+        zip: 37207
+    },
+    24: {
+        number: 1,
+        address: "130 Broadmoor Avenue",
+        city: "Nashville",
+        zip: 37207
+    },
+    25: {
+        number: 1,
+        address: "130 Broadmoor Avenue",
+        city: "Nashville",
+        zip: 37207
+    },
+    26: {
+        number: 1,
+        address: "130 Broadmoor Avenue",
+        city: "Nashville",
+        zip: 37207
+    },
+    27: {
+        number: 1,
+        address: "130 Broadmoor Avenue",
+        city: "Nashville",
+        zip: 37207
+    },
+    28: {
+        number: 1,
+        address: "130 Broadmoor Avenue",
+        city: "Nashville",
+        zip: 37207
+    },
+    29: {
+        number: 1,
+        address: "130 Broadmoor Avenue",
+        city: "Nashville",
+        zip: 37207
+    },
+    30: {
+        number: 1,
+        address: "130 Broadmoor Avenue",
+        city: "Nashville",
+        zip: 37207
+    },
+    31: {
+        number: 1,
+        address: "130 Broadmoor Avenue",
+        city: "Nashville",
+        zip: 37207
+    }
+
+}
+
---
+++ myapp/views.py
@@ -0,0 +1,81 @@
+from myapp import app
+from flask import jsonify, render_template, session, request
+from flask_socketio import SocketIO, send, emit
+from myapp import socketio
+from pymongo import MongoClient
+import json
+
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    print "->index()"
+    return render_template("incidentsPlot.html")
+
+
+@socketio.on('connect')
+def socketio_connet():
+    print "-> socketio_connect()"
+    socketio.emit("success")
+
+global cache_dictOut
+cache_dictOut = None
+
+@socketio.on('get_data')
+def getIncidentData():
+    global cache_dictOut
+
+    print "-> getIncidentData()"
+
+    '''
+    with open('myapp/static/get.json') as data_file:
+        print "------> data"
+        print data_file
+        socketio.emit("accident_data", {'data':json.load(data_file)})
+    '''
+    with open('dashboard.json') as data_file:
+        dash = json.load(data_file)
+    if not cache_dictOut:
+        client = MongoClient("mongodb://zilinwang:Mongo0987654321@129.59.107.60:27017/fire_department")
+        db = client["fire_department"]["incident"]
+        items = db.find()
+        dictOut = {}
+        dictArr = []
+        dictOut['incidents'] = dictArr
+
+        count = 0
+        for item in items:
+            count+=1
+            print count
+            if count>100:
+                break
+            dictIn = {}
+            dictIn['_id'] = str(item['_id'])
+            dictIn['incidentNumber'] = item['incidentNumber']
+            dictIn['_lat'] = item['latitude']
+            dictIn['_lng'] = item['longitude']
+            dictIn['year'] = item['year']
+            dictIn['month'] = item['month']
+            dictIn['day'] = item['calendarDay']
+            dictIn['alarmDate'] = item['alarmDateTime']
+            dictIn['closestStation'] = item['closestStation']
+            dictIn['severity'] = item['severity']
+            dictIn['streetNumber'] = item['streetNumber']
+            dictIn['streetPrefix'] = item['streetPrefix']
+            dictIn['streetName'] = item['streetName']
+            dictIn['streetType'] = item['streetType']
+            dictIn['streetSuffix'] = item['streetSuffix']
+            dictIn['apartment'] = item['apartment']
+            dictIn['city'] = item['city']
+            dictIn['county'] = item['county']
+            dictIn['state'] = item['state']
+            dictIn['zipCode'] = item['zipCode']
+
+            dictArr.append(dictIn)
+        cache_dictOut = dictOut
+
+     # jsonify(dictOut)
+    socketio.emit("accident_data", {'data': cache_dictOut})
+
+
