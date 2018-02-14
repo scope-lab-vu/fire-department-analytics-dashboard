d37bea779760dc1a2de82c80d0e2a37a2c7f77c3
ayan.mukhopadhyay <ayan.mukhopadhyay@vanderbilt.edu>
2018-02-14 08:56:54 -0600

    removed js debug

--- myapp/static/incidentscode.js
+++ myapp/static/incidentscode.js
@@ -1517,7 +1517,6 @@ socket.on('bestAreaInCharge', function(msg) {
 // set all predictions dots onto heat map
 var heatmapPredict;
 function setPredictions(arr) {
-    // alert("Inside set predictions");
     var heatMapData = [];
     for (var i = 0; i < arr.length; i++) {
         var obj = {};
@@ -1541,7 +1540,6 @@ function setPredictions(arr) {
 }
 
 function setPrediction(arr) {
-    // alert("Inside set prediction");
     var heatMapData = [];
     for (var i=0; i<arr.length; i++) {
         heatMapData.push(new google.maps.LatLng(arr[i][3], arr[i][4]));
