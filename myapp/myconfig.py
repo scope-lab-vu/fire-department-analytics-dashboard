5842e2a4c31d2c84dc1986072b9811ec2cbc290c
ayan.mukhopadhyay <ayan.mukhopadhyay@vanderbilt.edu>
2018-02-14 08:07:28 -0600

    nfd db ip

--- myapp/myconfig.py
+++ myapp/myconfig.py
@@ -1,5 +1,6 @@
 # change the MongoDB host and port
-MONGODB_HOST = 'mongodb://ayanmukg:mongopwdayan@localhost'
+#MONGODB_HOST = 'mongodb://ayanmukg:mongopwdayan@localhost'
 #MONGODB_HOST = 'mongodb://mongodb'
 #MONGODB_HOST = 'mongodb://ayanmukg:mongopwdayan@129.59.107.60'
+MONGODB_HOST = 'mongodb://ayanmukg:mongopwdayan@10.190.15.157'
 MONGODB_PORT = 27017
